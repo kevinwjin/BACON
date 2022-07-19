a5ac5141a0550ec5f636b26f60445af3ba08b18a
kevinwjin <kevin.jin@utdallas.edu>
2022-07-19 12:47:57 -0500

    More polygons

--- shape_generation.R
+++ shape_generation.R
@@ -4,5 +4,9 @@
 # Create empty canvas
 plot.new()
 
-# Draw simple polygon
+# Draw simple polygons
 polygon(c(0, 0, 0.2, 0.2), c(0, 0.7, 0.7, 0))
+
+polygon(c(0.3, 0.4, 0.5), c(0.05, 0.4, 0.05), lwd = 4) # line weight
+
+polygon(c(0, 0.1, 0.5, 0.5, 0.1), c(0.4, 0.5, 0.5, 0.3, 0.3), col = "green") # fill color
