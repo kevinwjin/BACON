28d54a9d78f905783a44d46eaf4255bd5824c4a6
kevinwjin <kevin.jin@utdallas.edu>
2022-09-12 17:04:54 -0500

    Angle vector and side length vector data generation

--- code/main.R
+++ code/main.R
@@ -1,28 +1,21 @@
 # Execution script (data generation, model testing)
 # Author: Kevin Jin
 
-# Preparation
+#### Preparation ####
 set.seed(13579)
-source("~/Documents/bsclust/code/shape_generation.R")
+source("~/Documents/Programming/Repositories/bsclust/code/shape_generation.R")
 
-# Test Rcpp functions
-library(Rcpp)
-sourceCpp("code/model.cpp")
-
-# Generate test shapes
-shape <- generate(k = 3)
+#### Test shape generation ####
+k <- 10 # Number of vertices
+shape <- generate(k = k)
 plot(shape, xlim = c(-2, 2), ylim = c(-2, 2), type = "l")
-
-polygon(jitter(shape, random = c("vertices"), factor = 0.01)) # Factor should be small
 #polygon(jitter(shape, random = c("angles"), factor = 0.01))
 polygon(translate(shape, x = -0.5, y = 1))
 polygon(dilate(shape, factor = 0.5))
-polygon(reflect(shape, direction = c("horizontal")))
 polygon(reflect(shape, direction = c("vertical")))
-polygon(rotate(shape, angle = 180, clockwise = TRUE))
 polygon(rotate(shape, angle = 180, clockwise = FALSE))
 
-# Multinomial angle vector-based triangle generation
+#### Multinomial angle vector-based triangle generation ####
 dmultinom(c(29, 61, 90), size = (sides - 2) * 180, prob = probs) # Density
 
 sides <- 3
@@ -32,61 +25,56 @@ triangles <- rmultinom(n = 100, # Number of polygons
                        prob = probs) # Probability for K angles
 t(triangles) # Generated vectors
 
-# Generate 30 equilateral and right triangles each with slight variations
-# Method 1: Generate 60 different triangles
-n <- 60 # Number of samples
-k <- 3 # Number of vertices
-shapes <- matrix(nrow = n * k, ncol = 2, byrow = TRUE) # Generated shapes
-for (i in seq(1, n * k, by = k)) { 
-  shapes[i:(i + (k - 1)), ] <- generate(k = k)
-}
-
-# Method 2: Replicate two triangles 5 times each and add jitter
-n <- 10 # Number of samples
+#### Generate data: Two different shapes, 30 each with slight variations ####
+## Step 1: Replicate two shapes 5 times each and add jitter
+n <- 60 # Total number of shapes
 k <- 3 # Number of vertices
 z <- 2 # Pre-specified number of clusters
 
-shape_1 <- generate(k = k) # Generate a triangle
-shapes_1 <- do.call(rbind, replicate(n / 2, shape_1, simplify = FALSE)) # Replicate n / 2 times
-for (i in seq(1, (n / 2 * (k + 1)), by = (k + 1))) { # Apply jitter (each 4 rows is a triangle)
+# Generate first shape
+shape_1 <- generate(k = k)
+# Replicate n / 2 times (half of the total)
+shapes_1 <- do.call(rbind, replicate(n / 2, shape_1, simplify = FALSE))
+# Apply jitter (each k + 1 rows is a shape)
+for (i in seq(1, (n / 2 * (k + 1)),
+              by = (k + 1))) {
   shapes_1[i:(i + k), ] <- jitter(shapes_1[i:(i + k), ],
                                         random = c("vertices"),
                                         factor = 0.01)
 }
-
-shape_2 <- generate(k = k) # Generate another triangle
-shapes_2 <- do.call(rbind, replicate(n / 2, shape_2, simplify = FALSE)) # Replicate n / 2 times
-for (i in seq(1, (n / 2 * (k + 1)), by = (k + 1))) { # Apply jitter
+# Generate second shape (z = 2 clusters)
+shape_2 <- generate(k = k)
+# Replicate n / 2 times (half of the total)
+shapes_2 <- do.call(rbind, replicate(n / 2, shape_2, simplify = FALSE))
+# Apply jitter (each k + 1 rows is a shape)
+for (i in seq(1, (n / 2 * (k + 1)), 
+              by = (k + 1))) {
   shapes_2[i:(i + k), ] <- jitter(shapes_2[i:(i + k), ],
                                         random = c("vertices"),
                                         factor = 0.01)
 }
+# Combine into 1 matrix
+shapes <- rbind(shapes_1, shapes_2)
 
-shapes <- rbind(shapes_1, shapes_2) # Combine into 1 matrix
-
-# Draw one triangle per plot
-count <- 0
-for (i in seq(1, (n / 2 * (k + 1)), by = (k + 1))) {
-  plot(shapes[i:(i + k), ], xlim = c(-2, 2), ylim = c(-2, 2), type = "l")
-  count <- count + 1
-  print(count)
+# Plot shapes - draw one shape per plot
+for (i in seq(1, (n / 2 * (k + 1)), 
+              by = (k + 1))) {
+  plot(shapes[i:(i + k), ], type = "l")
 }
 
-# Extract the angle vectors of the triangles (x, the data to be clustered)
-p <- 3 # Number of categories (length of angle vector)
+## Step 2: Extract the angle vectors of the shapes (data to be clustered)
+p <- k # Number of categories (length of angle vector)
 x <- matrix(nrow = n, ncol = p, byrow = TRUE) # Data (angle vectors)
-
-x_row <- 1 # Independent row counter
-for (i in seq(1, n * k, by = k)) {
-  x[x_row, ] <- get_interior_angles(shapes[i:(i + (k - 1)), ])
-  x_row <- x_row + 1
+row <- 1 # Row counter
+for (i in seq(1, n * (k + 1), by = (k + 1))) {
+  x[row, ] <- get_interior_angles(shapes[i:(i + k), ])
+  row <- row + 1
 }
-round(x) # Round to integers since we are treating the angle vector as a discrete variable
 
-# Extract side lengths of the triangles
-x_s <- matrix(nrow = n, ncol = p, byrow = TRUE) # Data (angle vectors)
-x_row <- 1 # Independent row counter
-for (i in seq(1, n * k, by = k)) {
-  x[x_row, ] <- get_side_lengths(shapes[i:(i + (k - 1)), ])
-  x_row <- x_row + 1
+## Step 3: Extract side lengths of the shapes (data to be clustered)
+s <- matrix(nrow = n, ncol = p, byrow = TRUE) # Data (side length vectors)
+row <- 1 # Row counter
+for (i in seq(1, n * (k + 1), by = (k + 1))) {
+  s[row, ] <- get_side_lengths(shapes[i:(i + k), ])
+  row <- row + 1
 }
