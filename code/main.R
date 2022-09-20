3a0ec8a7640dbfc763a5002edf82c0a7c55697be
kevinwjin <kevin.jin@utdallas.edu>
2022-09-20 14:32:09 -0500

    ADHD-200 data loading procedure

--- code/main.R
+++ code/main.R
@@ -64,3 +64,51 @@ for (i in seq(1, n * (k + 1), by = (k + 1))) {
   s[row, ] <- get_side_lengths(shapes[i:(i + k), ])
   row <- row + 1
 }
+
+#### Load ADHD-200 data ####
+# Test data extraction
+chain <- plg.chains[[1]] # Unlist polygonal chain
+colnames(chain) <- c("x", "y") # Rename columns, otherwise sf will complain
+k <- nrow(chain) - 1 # Number of vertices
+plot(chain, type = "l")
+text(chain, labels = 1:nrow(chain))
+get_interior_angles(chain)
+get_side_lengths(chain)
+
+# Load all chains in the folder
+setwd("~/Documents/Programming/Repositories/CAPoly/data/adhd_200/shapes/polygonal")
+file_names <- list.files(pattern = "*.Rdata", full.names = TRUE)
+chains <- lapply(file_names, function(x) {
+  load(file = x)
+  mget(ls()[ls()!= "filename"])
+})
+
+# Construct matrices for data
+angles <- matrix(nrow = length(chains), ncol = 50, byrow = TRUE)
+side_lengths <- matrix(nrow = length(chains), ncol = 50, byrow = TRUE)
+
+# Extract data
+for (i in 1:length(chains)) {
+  chain <- chains[[i]]$plg.chains$`1` # Extract chain
+  colnames(chain) <- c("x", "y")
+  angles[i, ] <- get_interior_angles(chain)
+  side_lengths[i, ] <- get_side_lengths(chain)
+}
+
+# Name data rows and columns
+names <- sapply(1:length(chains), function(i) chains[[i]]$id[2])
+rownames(angles) <- names
+rownames(side_lengths) <- names
+colnames(angles) <- 1:50
+colnames(side_lengths) <- 1:50
+
+#### Load MPEG-7 data ####
+# Load all images in the folder
+
+# Convert to polygonal chains
+
+# Construct matrices for data
+
+# Extract data
+
+# Name data rows and columns
